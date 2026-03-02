38c21869443fdcf990c544a117031e3a7aefa852
Prothom <prothomghosh41@gmail.com>
2026-03-02 22:44:52 +0600

    Project Initialization

---
+++ src/main/java/com/example/book_exchange_sepm/BookExchangeSepmApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_exchange_sepm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookExchangeSepmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookExchangeSepmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/book_exchange_sepm/BookExchangeSepmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_exchange_sepm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookExchangeSepmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
